0fb383060eba43c07ca38b32a65f4a5206065360
Fomina Anna <annafomina01@gmail.com>
2020-12-15 05:20:10 +0300

    files in menu

---
+++ functions/func.php
@@ -0,0 +1,26 @@
+
+<?php
+function print_info($query)
+{
+    $link = mysqli_connect("localhost", "root", "", "cafe")
+    or die("Ошибка " . mysqli_error($link));
+
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+    if ($result) {
+        $rows = mysqli_num_rows($result);
+        echo "<table border='1'><tr><th>Наименование</th><th>Цена</th></tr>";
+        for ($i = 0; $i < $rows / 3; ++$i) {
+            $row = mysqli_fetch_row($result);
+            echo "<tr>";
+            for ($j = 0; $j < 2; ++$j) echo "<td>$row[$j]</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+
+        // очищаем результат
+        mysqli_free_result($result);
+    }
+}
+
+		?>
+
